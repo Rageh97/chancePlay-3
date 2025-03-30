c02332e5dcb80e1cb1f655f3f210854579e1879e
Rageh97 <abdelhady412rageh@gmail.com>
2025-03-30 23:44:44 +0200

    commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  content: ["./category.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+      },
+      animation: {
+        fadeIn: "fade-in 1s ease-out forwards", // ← اربط الكي فريم هنا
+      },
+    },
+  },
+  plugins: [require("tailwindcss-filters")],
+};
